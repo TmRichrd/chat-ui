338644fe16af43a9bd05e475dbf1d35fc71554c2
Tang_m <1084491297@qq.com>
2023-05-03 01:18:43 +0800

    init🎉: 搭建基本架构，添加头像组件

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components';
+import theme from "../src/theme"
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ src/components/Avatar/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import StyledAvatar, { StatusIcon, AvatarClip, AvatarImage } from './style'
+function Avatar ({
+  src,
+  size = '48px',
+  status,
+  statusIconSize = '8px',
+  ...rest
+}) {
+  return (
+    <StyledAvatar {...rest}>
+      {status && <StatusIcon status={status} size={statusIconSize}></StatusIcon>}
+      <AvatarClip size={size}>
+        <AvatarImage src={src} alt="" />
+      </AvatarClip>
+    </StyledAvatar>
+  )
+}
+
+Avatar.propTypes = {}
+
+export default Avatar
